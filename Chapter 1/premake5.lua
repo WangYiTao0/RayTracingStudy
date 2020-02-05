231bfb7b9e79d708cbc3a0630ee98bd25edae73d
Wang <as5818956@gmail.com>
2020-02-06 03:26:27 +0900

    1

---
+++ Chapter 1/premake5.lua
@@ -0,0 +1,50 @@
+project "Chapter 1"
+kind "ConsoleApp"
+language "c++"
+cppdialect "c++17"
+staticruntime "on"
+
+targetdir("bin/" .. outputdir .. "%{prj.name}")
+objdir("bin-obj/" .. outputdir .. "%{prj.name}")
+
+--pchheader "aspch.h"
+--pchsource "src/aspch.cpp"
+
+files
+{
+    "**.h",
+    "**.cpp"
+}
+
+defines
+{
+    "_CRT_SECURE_NO_WARNINGS"
+}
+
+includedirs
+{
+    "src"
+}
+
+links
+{
+
+}
+
+filter "system:windows"
+    systemversion "latest"
+
+    defines
+    {
+
+    }
+
+filter "configurations:Debug"
+    defines "AS_DEBUG"
+    runtime "Debug"
+    symbols "on"
+
+filter "configurations:Release"
+    defines "AS_RELEASE"
+    runtime "Release"
+    optimize "on"
