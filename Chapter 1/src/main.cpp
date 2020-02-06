45ca89e5b9114217d26b146a9f84c03cab4e3466
wyt <as5818956@gmail.com>
2020-02-06 13:51:32 +0900

    Chapter add vec3.h

--- Chapter 1/src/main.cpp
+++ Chapter 1/src/main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <fstream>
 
+#include "vec3.h"
+
 int main()
 {
 	std::ofstream outputFile;
@@ -17,12 +19,11 @@ int main()
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2f);
+
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
 			outputFile << ir << " " << ig << " " << ib << "\n";
 		}
 	}
